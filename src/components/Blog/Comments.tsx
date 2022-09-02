db684d72d96e1b938a9c35a70f7c8b756ce1262e
Artun Çolak <artuncolak97@gmail.com>
2022-09-03 02:11:17 +0300

    Line added to comments header

--- src/components/Blog/Comments.tsx
+++ src/components/Blog/Comments.tsx
@@ -1,28 +1,38 @@
 import Giscus from '@giscus/react';
+import classNames from 'classnames';
 import { useTheme } from 'next-themes';
 
 export default function Comments() {
   const { theme } = useTheme();
-
-  if (process.env.NODE_ENV !== 'production') return null;
+  const isProduction = process.env.NODE_ENV === 'production';
 
   return (
     <div className="mt-16">
-      <h1 className="mb-5 sm:text-4xl">Leave a comment</h1>
-      <Giscus
-        repo="artuncolak/artuncolak.com-comments"
-        repoId="R_kgDOH7A82w"
-        category="Announcements"
-        categoryId="DIC_kwDOH7A8284CRLXk"
-        mapping="pathname"
-        strict="0"
-        reactionsEnabled="1"
-        emitMetadata="0"
-        inputPosition="top"
-        theme={theme === 'light' ? 'light' : 'transparent_dark'}
-        lang="en"
-        loading="lazy"
-      />
+      <h1
+        className={classNames(
+          'mb-5 flex items-center gap-5 sm:text-4xl',
+          'after:block after:h-px after:flex-1 after:bg-zinc-200 dark:after:bg-zinc-700',
+          'before:hidden before:h-px before:bg-zinc-200 dark:before:bg-zinc-700 sm:before:block sm:before:w-10'
+        )}
+      >
+        Leave a comment
+      </h1>
+      {isProduction && (
+        <Giscus
+          repo="artuncolak/artuncolak.com-comments"
+          repoId="R_kgDOH7A82w"
+          category="Announcements"
+          categoryId="DIC_kwDOH7A8284CRLXk"
+          mapping="pathname"
+          strict="0"
+          reactionsEnabled="1"
+          emitMetadata="0"
+          inputPosition="top"
+          theme={theme === 'light' ? 'light' : 'transparent_dark'}
+          lang="en"
+          loading="lazy"
+        />
+      )}
     </div>
   );
 }
