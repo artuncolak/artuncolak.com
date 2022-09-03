05d7dbea242e4bcd32dc5b22aca601d5c4987801
Artun Çolak <artuncolak97@gmail.com>
2022-09-03 17:36:04 +0300

    Favicons added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import Layout from '@components/Layout';
 import { ENVIRONMENT, SPLITBEE_TOKEN } from '@lib/config';
 import splitbee from '@splitbee/web';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { ThemeProvider } from 'next-themes';
 import { useEffect } from 'react';
 
@@ -15,10 +16,32 @@ export default function App({ Component, pageProps }: AppProps) {
   }, []);
 
   return (
-    <ThemeProvider attribute="class">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+      </Head>
+      <ThemeProvider attribute="class">
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
   );
 }
