21fde28ab366b451ad3c01cd7f76cfec5576c6d2
Artun Çolak <artuncolak97@gmail.com>
2022-09-03 17:20:18 +0300

    Splitbee analytics added

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,5 @@
+export const ENVIRONMENT = process.env.NODE_ENV;
+
 //Website
 export const WEBSITE_DOMAIN = String(process.env.NEXT_PUBLIC_WEBSITE_DOMAIN);
 
@@ -7,6 +9,9 @@ export const GISCUS_REPO_ID = String(process.env.NEXT_PUBLIC_GISCUS_REPO_ID);
 export const GISCUS_CATEGORY_ID = String(
   process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID
 );
+
+//Splitbee
+export const SPLITBEE_TOKEN = String(process.env.NEXT_PUBLIC_SPLITBEE_TOKEN);
 
 //Spotify
 export const SPOTIFY_CLIENT_ID = String(process.env.SPOTIFY_CLIENT_ID);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
 import '@styles/global.css';
 
 import Layout from '@components/Layout';
+import { ENVIRONMENT, SPLITBEE_TOKEN } from '@lib/config';
+import splitbee from '@splitbee/web';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (ENVIRONMENT === 'production') {
+      splitbee.init({ token: SPLITBEE_TOKEN });
+    }
+  }, []);
+
   return (
     <ThemeProvider attribute="class">
       <Layout>
